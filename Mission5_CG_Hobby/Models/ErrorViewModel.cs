07175b73f330cb44aaa97eeb0903f9c4e2eb78c3
Charlotte Griffin <charlottegriffin11@gmail.com>
2024-02-09 12:12:23 -0700

    Add project files.

---
+++ Mission5_CG_Hobby/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission5_CG_Hobby.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
