7251c1acbbc9e36ce88a40a80a03fe1561536cbf
Charlotte Griffin <charlottegriffin11@gmail.com>
2024-02-09 17:07:12 -0700

    Officially done for reals this time I swear.

--- Mission5_CG_Hobby/wwwroot/js/site.js
+++ Mission5_CG_Hobby/wwwroot/js/site.js
@@ -6,6 +6,10 @@
 
 $("#btnClick").on('click', function () {
     var hours = parseFloat($("#hours").val());
+    if (hours <= 0) {
+        alert("Number of hours must be more than 0. ");
+        $("#hours").val() = 1;
+    }
     var rate = parseFloat($("#rate").val());
     var result = hours * rate;
 
